8a90e93b466a8f3f6e006d9ce6204a9efb279866
Ionathan Sideras <iona8ansideras@gmail.com>
2025-07-23 19:53:11 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function PhysicalTherapistWebsite() {
     return (
-        <div className="min-h-screen ">
+        <div className="min-h-screen">
             <main>
                 <Header />
 
